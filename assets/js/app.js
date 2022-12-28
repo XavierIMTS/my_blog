8d0013e2bcde8d9eb7cdc5d4076b0acfd8857275
XavierIMTS <114238634+XavierIMTS@users.noreply.github.com>
2022-12-28 16:15:58 +0100

    Create_template_Base

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,5 +7,23 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.scss';
+import {Dropdown} from "bootstrap";
+
+document.addEventListener('DOMContentLoaded', () => {
+    enableDropdowns();
+} );
 
 
+const enableDropdowns = () => {
+    
+    //alert("js");
+    const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+    dropdownElementList.map(function (dropdownToggleEl) {
+      return new Dropdown(dropdownToggleEl);
+    });
+ 
+    /*
+    const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+      const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
+    */
+}
