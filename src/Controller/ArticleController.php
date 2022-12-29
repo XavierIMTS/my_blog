c3dc6b412b16e5a924b27b5d955f5c178aab9a51
XavierIMTS <114238634+XavierIMTS@users.noreply.github.com>
2022-12-29 13:47:15 +0100

    Creation du système d'articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
     #[Route('/article/{slug}', name: 'article_show')]
+    /*
     public function show(): Response
     {
         return $this->render('article/show.html.twig', [
             'controller_name' => 'ArticleController',
+        ]);
+    }
+    */
+
+    public function show(?Article $article): Response
+    {
+        if(!$article){
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
         ]);
     }
 }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -2,14 +2,16 @@
 
 namespace App\Entity;
 
-use App\Repository\ArticleRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use DateTimeInterface;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Model\TimestampedInterface;
+use App\Repository\ArticleRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
-class Article
+class Article implements TimestampedInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -117,24 +119,24 @@ class Article
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeInterface
+    public function getCreatedAt(): DateTimeInterface
     {
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    public function setCreatedAt(? DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
 
         return $this;
     }
 
-    public function getUpdatedAt(): ?\DateTimeInterface
+    public function getUpdatedAt(): DateTimeInterface
     {
         return $this->updatedAt;
     }
 
-    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    public function setUpdatedAt(? DateTimeInterface $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
 
